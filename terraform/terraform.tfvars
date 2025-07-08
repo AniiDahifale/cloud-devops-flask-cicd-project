d31225479afd93eabbae1287e7ca85825749967e
Aniket <Aniket1@gmail.com>
2025-07-08 23:21:21 +0530

    added configuration of rest files

---
+++ terraform/terraform.tfvars
@@ -0,0 +1,3 @@
+resource_group_name = "aniket-devops-rg"
+location            = "East US"
+aks_name            = "aniket-aks-cluster"
---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "resource_group_name" {
+  default = "flask-rg"
+}
+
+variable "location" {
+  default = "UK South"
+}
+
+variable "aks_name" {
+  default = "devops-aks-cluster"
+}
