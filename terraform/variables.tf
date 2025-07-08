33ed1fd87a1efae8eb6f8846bba77a5e571d3c4a
Aniket <Aniket1@gmail.com>
2025-07-08 23:25:32 +0530

    added configuration of rest files

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,14 @@
 variable "resource_group_name" {
+  type = string  
   default = "flask-rg"
 }
 
 variable "location" {
+  type = string  
   default = "UK South"
 }
 
 variable "aks_name" {
+  type = string  
   default = "devops-aks-cluster"
 }
